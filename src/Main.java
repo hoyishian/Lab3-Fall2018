51783959c2f8c9cdd0024ee35a878c509e2179aa
hoyishian <ysho2@illinois.edu>
2018-09-19 12:27:06 -0500

    initial commit.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,14 @@
+
+
+/**
+ * Retrieve contents from a URL and return them as a string.
+ *
+ * @param url url to retrieve contents from
+ * @return the contents from the url as a string, or an empty string on error
+ */
+
+
+
 public class Main {
 
     public static void main(String[] args) {
--- src/WebSplitter.java
+++ src/WebSplitter.java
@@ -1,3 +1,19 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Main {
+public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+        urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+        return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+        }
 public class WebSplitter {
     public static void main(String[] args) {
 
